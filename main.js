8a060226ea5a2d84a1764fffe1134f4b1a78fc74
josephtmartin <goldglovejo13@gmail.com>
2020-08-01 12:27:17 -0500

    made cards print adn expel button works

--- main.js
+++ main.js
@@ -1,25 +1,6 @@
 'use strict';
 
-const formName = [''];
-
-const hogwartsHouses = [
-  {
-    studentName: 'Harry Potter',
-    houseName: 'Gryffindor',
-  },
-  {
-    studentName: 'Silvanus Kettleburn',
-    houseName: 'Hufflepuff',
-  },
-  {
-    studentName: 'Filius Flitwick',
-    houseName: 'Ravenclaw',
-  },
-  {
-    studentName: 'Draco Malfoy',
-    houseName: 'Slytherin',
-  },
-];
+const hatCards = [];
 
 const handleIntroButtonClick = (e) => {
     document.getElementById('sortingForm').style.display = 'block';
@@ -30,19 +11,26 @@ const handleSubmitButtonClick = (e) => {
 
   const houses = ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin'];
   let randomHouse = houses[Math.floor(Math.random() * houses.length)];
-  console.log('inputName:', inputName);
-  console.log('randomHouse:', randomHouse);
-  let obj = {};
+
+  let obj = {
+    studentName: inputName,
+    houseName: randomHouse,
+  };
+
+  hatCards.push(obj);
+  cardBuilder(hatCards);
 };
 
+const handleExpelButtonClick = (e) => {
+    const target = e.target.id;
+    hatCards.splice(target, 1);
+    cardBuilder(hatCards);
+}
+
 const buttonEvents = () => {
-  document
-    .querySelector('#submit')
-    .addEventListener('click', handleSubmitButtonClick);
-  document
-    .querySelector('#introBtn')
-    .addEventListener('click', handleIntroButtonClick);
-    
+  document.querySelector('#introBtn').addEventListener('click', handleIntroButtonClick);
+  document.querySelector('#submit').addEventListener('click', handleSubmitButtonClick);
+  document.querySelector('#cards').addEventListener('click', handleExpelButtonClick);
 };
 
 const printToDom = (divId, textToPrint) => {
@@ -54,12 +42,12 @@ const cardBuilder = (arr) => {
   let domString = '';
 
   for (let i = 0; i < arr.length; i++) {
-    domString += `<div class="card" style="width: 18rem;">
+    domString += `<div class="card" style="width: 18rem;" id="${i}>
                     <img class="card-img-top" src=".../100px180/" alt="Card image cap">
                         <div class="card-body">
-                            <h5 class="card-title">${hogwartsHouses[i].studentName}</h5>
-                            <p class="card-text">${hogwartsHouses[i].houseName}</p>
-                            <a href="#" class="btn btn-danger">Expel</a>
+                            <h5 class="card-title">${hatCards[i].studentName}</h5>
+                            <p class="card-text">${hatCards[i].houseName}</p>
+                            <a href="#" class="btn btn-danger" id="${i}">Expel</a>
                         </div>
                     </div>`;
   }
@@ -68,7 +56,7 @@ const cardBuilder = (arr) => {
 };
 
 const init = () => {
-  cardBuilder(hogwartsHouses);
+  cardBuilder(cards);
   buttonEvents();
 };
 
